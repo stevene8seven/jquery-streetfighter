8be7e73098613f4ba0c2c99b98108a7ac32349b9
stevene8seven <scuba.7.steve@gmail.com>
2015-08-28 23:59:39 -0500

    just need to add keydown()

--- js/app.js
+++ js/app.js
@@ -11,18 +11,36 @@ $('.ryu').mouseenter(function() {
   })
 
 
-  .mousedown(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // play hadouken sound
+ .mousedown(function() {
+  playHadouken();
+  $('.ryu-ready').hide();
+  $('.ryu-throwing').show();
+  $('.hadouken').finish().show()
+  .animate(
+    {'left': '1020px'},
+    500,
+    function() {
+      $(this).hide();
+      $(this).css('left', '520px');
+    });
+})
+   
     // show hadouken and animate it to the right of the screen
-  })
 
   .mouseup(function() {
     $('.ryu-throwing').hide();
   	$('.ryu-ready').show();
     // ryu goes back to his ready position
 
+	});
+
+ 
+
+
 });
-});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
