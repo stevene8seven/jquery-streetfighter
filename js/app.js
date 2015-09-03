dbacb160666ab465ff50ed8cd064b4893e12ec2c
stevene8seven <scuba.7.steve@gmail.com>
2015-09-02 22:11:16 -0500

    final submission

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
-$(document).ready(function() {
 
 $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-	$('.ryu-ready').show();
-  })
+  $('.ryu div').hide();
+  $('.ryu-ready').show();
+})
 
-.mouseleave(function() {
-    $('.ryu-ready').hide();
-	$('.ryu-still').show();
-  })
+  .mouseleave(function() {
+  $('.ryu div').hide();
+  $('.ryu-still').show();
+})
 
 
- .mousedown(function() {
+  .mousedown(function() {
   playHadouken();
-  $('.ryu-ready').hide();
+  $('.ryu div').hide();
   $('.ryu-throwing').show();
   $('.hadouken').finish().show()
-  .animate(
+    .animate(
     {'left': '1020px'},
     500,
     function() {
@@ -24,20 +23,29 @@ $('.ryu').mouseenter(function() {
       $(this).css('left', '520px');
     });
 })
-   
-    // show hadouken and animate it to the right of the screen
+
+// show hadouken and animate it to the right of the screen
 
   .mouseup(function() {
-    $('.ryu-throwing').hide();
-  	$('.ryu-ready').show();
-    // ryu goes back to his ready position
-
-	});
-
- 
-
+  $('.ryu div').hide();
+  $('.ryu-ready').show();
+  // ryu goes back to his ready position
 
 });
+
+$(document).keydown(function(event){
+  if (event.which === 88){
+    $('.ryu div').hide();
+    $('.ryu-cool').show();
+  } 
+
+}).keyup(function(event){
+  if(event.which === 88){
+    $('.ryu div').hide();
+    $('.ryu-still').show();
+  }
+});
+
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
